5bbc5ac01f96d1bba27100e2c82b1b04a9c9162a
Endy Muhardin <endy.muhardin@gmail.com>
2013-12-04 10:38:18 +0700

    implement connect/disconnect

--- pictag-web/src/main/java/com/muhardin/endy/pictag/dao/UserDao.java
+++ pictag-web/src/main/java/com/muhardin/endy/pictag/dao/UserDao.java
@@ -1,10 +1,14 @@
 package com.muhardin.endy.pictag.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.List;
 import training.pictag.domain.User;
 
 public class UserDao {
     private DatabaseConfiguration configuration;
+    private Connection databaseConnection;
     
     public UserDao(String databaseDriver, String databaseUrl, 
             String databaseUsername, String databasePassword){
@@ -22,12 +26,29 @@ public class UserDao {
         configuration = config;
     }
     
-    public void connect(){
-    
+    public void connect() throws ClassNotFoundException {
+        try {
+            // 1. load database driver
+            Class.forName(configuration.getDatabaseDriverName());
+
+            // 2. connect to database
+            databaseConnection = DriverManager.getConnection(
+                    configuration.getDatabaseUrl(), 
+                    configuration.getDatabaseUsername(), 
+                    configuration.getDatabasePassword());
+        } catch (SQLException err){
+            System.out.println("Error connecting to database");
+            System.out.println("Error description : "+err.getMessage());
+        }
     }
     
     public void disconnect(){
-    
+        try {
+            databaseConnection.close();
+        } catch (SQLException err) {
+            System.out.println("Error disconnecting to database");
+            System.out.println("Error description : "+err.getMessage());
+        }
     }
     
     public void insert(User u){
