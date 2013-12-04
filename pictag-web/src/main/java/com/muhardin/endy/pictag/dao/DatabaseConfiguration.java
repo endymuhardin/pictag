7cd193672c25fa1d8901f06d19f48514b4ba871b
Endy Muhardin <endy.muhardin@gmail.com>
2013-12-04 08:49:27 +0700

    skeleton method for managing user data in database

---
+++ pictag-web/src/main/java/com/muhardin/endy/pictag/dao/DatabaseConfiguration.java
@@ -0,0 +1,42 @@
+package com.muhardin.endy.pictag.dao;
+
+class DatabaseConfiguration {
+    private String databaseDriverName;
+    private String databaseUrl;
+    private String databaseUsername;
+    private String databasePassword;
+
+    public String getDatabaseDriverName() {
+        return databaseDriverName;
+    }
+
+    public void setDatabaseDriverName(String databaseDriverName) {
+        this.databaseDriverName = databaseDriverName;
+    }
+
+    public String getDatabaseUrl() {
+        return databaseUrl;
+    }
+
+    public void setDatabaseUrl(String databaseUrl) {
+        this.databaseUrl = databaseUrl;
+    }
+
+    public String getDatabaseUsername() {
+        return databaseUsername;
+    }
+
+    public void setDatabaseUsername(String databaseUsername) {
+        this.databaseUsername = databaseUsername;
+    }
+
+    public String getDatabasePassword() {
+        return databasePassword;
+    }
+
+    public void setDatabasePassword(String databasePassword) {
+        this.databasePassword = databasePassword;
+    }
+    
+    
+}
