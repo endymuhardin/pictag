1b355b11ed5b22138858ae2795f5909cb9c2c7b9
Endy Muhardin <endy.muhardin@gmail.com>
2013-12-10 09:40:55 +0700

    login screen

---
+++ pictag-android/src/main/java/com/muhardin/endy/pictag/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.muhardin.endy.pictag;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by endy on 12/10/13.
+ */
+public class LoginActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
