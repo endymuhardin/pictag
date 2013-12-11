ce19dfcd694503a5a8eb7f8713a7386bffd687f7
Endy Muhardin <endy.muhardin@gmail.com>
2013-12-11 08:33:09 +0700

    data transfer object for login flow

---
+++ pictag-common/src/main/java/training/pictag/dto/PictagServerResponse.java
@@ -0,0 +1,25 @@
+package training.pictag.dto;
+
+/**
+ * Created by endy on 12/11/13.
+ */
+public class PictagServerResponse {
+    private Boolean success;
+    private String message;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
